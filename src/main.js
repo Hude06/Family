b6652510c389e424c7d5c3623e7475fce5a09a2a
judeallanhill <judeallanhill@icloud.com>
2024-07-26 08:56:51 -0400

    adding some more stuff to get to the user emails

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ login.addEventListener("click", async function () {
   signInWithGithub();
 });
 let logedInAs = await userStatus();
-console.log(logedInAs.data);
+console.log(logedInAs.data.user.email);
 async function initGroups() {
   let groups = await fetchData();
   for (let i = 0; i < groups.length; i++) {

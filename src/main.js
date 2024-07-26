3a9c74757d7233650eafca25ab642da05583385c
judeallanhill <judeallanhill@icloud.com>
2024-07-26 19:06:43 -0400

    adding osm

--- src/main.js
+++ src/main.js
@@ -15,6 +15,22 @@ let texting = true;
 familyBUTT.addEventListener("click", async function () {
   currentGroup = "family";
 });
+function initMap() {
+  // Initialize the map and set its view to the desired geographical coordinates
+  var map = L.map("map").setView([51.505, -0.09], 13); // Coordinates for London with zoom level 13
+
+  // Add a tile layer to the map (this is where the OSM data is fetched from)
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  // Optionally, add a marker
+  L.marker([51.5, -0.09])
+    .addTo(map)
+    .bindPopup("A marker in London.")
+    .openPopup();
+}
 function loop() {
   if (currentGroup === "family") {
     familypage.style.display = "block";
